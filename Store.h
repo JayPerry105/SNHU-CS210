8eaa855762a6b0311159dc1b56eb5555571e97e2
JayPerry105 <124695649+JayPerry105@users.noreply.github.com>
2023-02-08 20:07:03 -0500

    Update Store.h

--- Store.h
+++ Store.h
@@ -1,18 +1,5 @@
 #pragma once
 class Store {
-    string nCharString(size_t n, char c);
-
-
-    string nCharString(size_t n, char c) {
-        int i;
-        string charStr;
-
-        for (i = 0; i < n; ++i) {
-            charStr += c;
-        }
-        return charStr;
-    }
-    
-    
+    public:
 };
 
