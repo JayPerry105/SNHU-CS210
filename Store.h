7938edcbd16665617b45caea87c583cb4b32bfc3
JayPerry105 <124695649+JayPerry105@users.noreply.github.com>
2023-02-13 11:45:07 -0500

    Update Store.h

--- Store.h
+++ Store.h
@@ -21,18 +21,18 @@ private:
 	string charStr;
 };
 
-void Store::PrintMenu() const {
-		cout << "Select an option by entering 1, 2, 3, or 4. " << endl << endl;
-		cout << "Option 1: Enter item you wish to search for." << endl;
-		cout << "Option 2: Print list of items with frequncy of purchases." << endl;
-		cout << "Option 3: Print list of items with frequency of purchases as histrogram." << endl;
-		cout << "Option 4: Exit the program." << endl;
+void Store::PrintMenu() const { // Displays a default menu to the user when prompting for user input
+	cout << "Select an option by entering 1, 2, 3, or 4. " << endl << endl;
+	cout << "Option 1: Enter item you wish to search for." << endl;
+	cout << "Option 2: Print list of items with frequncy of purchases." << endl;
+	cout << "Option 3: Print list of items with frequency of purchases as histrogram." << endl;
+	cout << "Option 4: Exit the program." << endl;
 }
 
-string Store::nCharString(int size, char symb) {
+string Store::nCharString(int size, char symb) { // Function to allow us to print any character 'n' amount of times
 	n = size;
 	c = symb;
-
+	charStr = "";
 	for (i = 0; i < n; ++i) {
 		charStr += c;
 	}
